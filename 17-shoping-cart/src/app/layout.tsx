5de12b4f54f22d361d5a073e9223f0ba9daece6f
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-05-04 11:21:21 -0400

    shoping cart

---
+++ 17-shoping-cart/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import { inter } from '@/config/fonts';
+
+
+
+import './globals.css';
+
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Teslo | Shop',
+    default: 'Home - Teslo | Shop'
+  },
+  description: 'Una tienda virtual de productos',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ 17-shoping-cart/src/store/index.ts
@@ -0,0 +1,5 @@
+
+
+
+export * from './cart/cart-store';
+export * from './ui/ui-store';
